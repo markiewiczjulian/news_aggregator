484c3a77baca76f7e9e48a511b8c448475aad7ad
markiewiczjulian <markiewicz.julian@gmail.com>
2019-11-02 20:40:27 +0100

    added tests to sources component

--- src/app/sources-component/sources-component.spec.ts
+++ src/app/sources-component/sources-component.spec.ts
@@ -3,39 +3,51 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { HttpClient, HttpHandler } from '@angular/common/http';
 
 import { SourcesComponent } from './sources-component';
+import { of } from 'rxjs';
+import { SourceNewsService } from '../shared/services/source-news.service';
+import { SourceResponse } from '../shared/models/sourceResponse';
+import { Source } from '../shared/models/source';
 
 describe('SourcesComponent', () => {
-  let component: SourcesComponent;
+  let sourcesComponent: SourcesComponent;
   let fixture: ComponentFixture<SourcesComponent>;
+  let sourcesService: SourceNewsService;
+  const source: Source[] = [{
+    id: '1',
+    name: 'name',
+    description: 'description',
+    url: '1111.com',
+    category: '3',
+    language: 'EN',
+    country: 'US'
+  }];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SourcesComponent],
-      providers: [HttpClientTestingModule, HttpTestingController, HttpClient, HttpHandler]
+      providers: [HttpClientTestingModule, HttpTestingController, HttpClient,
+        HttpHandler, SourceNewsService]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SourcesComponent);
-    component = fixture.componentInstance;
-    component.ngOnInit = () => { };
+    sourcesComponent = fixture.componentInstance;
+    sourcesService = TestBed.get(SourceNewsService)
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(sourcesComponent).toBeTruthy();
   });
 
-  //TODO test method getAllSources and redirectToSourceSite
-  xit(`method getAllSources should call sourcesService and assign retrieved data to
+  it(`method getAllSources should call sourcesService and assign retrieved data to
   sources variable`, () => {
-    const service: SourcesComponent = TestBed.get(SourcesComponent);
-    expect(service).toBeTruthy();
-  });
-
-  xit(`method redirectToSourceSite should change location of site to passed url`, () => {
-    const service: SourcesComponent = TestBed.get(SourcesComponent);
-    expect(service).toBeTruthy();
+    let spy = spyOn(sourcesService, 'getAll').and.returnValue(of(source));
+    sourcesComponent.getAllSources();
+    expect(spy).toHaveBeenCalled();
+    expect(sourcesComponent.sources).toBeTruthy();
+    expect(sourcesComponent.sources[0].id).toBe('1');
   });
 });
